fb6af85003c3c0134cfe3a135fcb9d4bac854dca
Connor Mooney-Collett <connor.mooneycollett@gmail.com>
2020-12-01 16:29:59 +1100

    Solved Day 1, Parts 1 and 2

---
+++ src/day_01.rs
@@ -0,0 +1,52 @@
+#[aoc_generator(day1)]
+fn generate_input(input: &str) -> Vec<u64> {
+    let mut output: Vec<u64> = vec![];
+    for line in input.lines() {
+        let line = line.trim();
+        if line.is_empty() {
+            continue;
+        }
+        let value = line.parse::<u64>().unwrap();
+        output.push(value);
+    }
+    return output;
+}
+
+#[aoc(day1, part1)]
+fn solve_part_1(values: &Vec<u64>) -> u64 {
+    for i in 0..values.len() {
+        // Check values ahead of first index, to prevent duplication of work
+        for j in (i+1)..values.len() {
+            if values[i] + values[j] == 2020 {
+                return values[i] * values[j];
+            }
+        }
+    }
+    panic!("Day 1 Part 1 - should not get here!");
+}
+
+#[aoc(day1, part2)]
+fn solve_part_2(values: &Vec<u64>) -> u64 {
+    for i in 0..values.len() {
+        for j in (i + 1)..values.len() {
+            for k in (j + 1)..values.len() {
+                if values[i] + values[j] + values[k] == 2020 {
+                    return values[i] * values[j] * values[k];
+                }
+            }
+        }
+    }
+    panic!("Day 1 Part 2 - should not get here!");
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_d01_p1_proper() {
+        let input = generate_input(&std::fs::read_to_string("./input/2020/day1.txt").unwrap());
+        let result = solve_part_1(&input);
+        assert_eq!(996075, result);
+    }
+}
--- src/lib.rs
+++ src/lib.rs
@@ -4,5 +4,6 @@ extern crate aoc_runner_derive;
 pub mod utils;
 
 // Modules for day solutions
+pub mod day_01;
 
 aoc_lib!{ year = 2020 }

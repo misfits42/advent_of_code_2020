0b207740c5a870c0547c61994acd118ef306d51f
Connor Mooney-Collett <connor.mooneycollett@gmail.com>
2020-12-02 19:10:48 +1100

    Tidied up Day 2 code

--- src/day_02.rs
+++ src/day_02.rs
@@ -1,30 +1,32 @@
 use regex::Regex;
 
 #[aoc_generator(day2)]
-fn generate_input(input: &str) -> Vec<(usize, usize, String, String)> {
-    let mut output: Vec<(usize, usize, String, String)> = vec![];
+fn generate_input(input: &str) -> Vec<(usize, usize, char, Vec<char>)> {
+    let mut output: Vec<(usize, usize, char, Vec<char>)> = vec![];
     let password_regex = Regex::new(r"(\d+)-(\d+) ([a-z]): ([a-z]+)").unwrap();
     for line in input.lines() {
+        // Trim leading and training whitespace, then ignore empty lines
         let line = line.trim();
         if line.is_empty() {
             continue;
         }
+        // Match line against regex, then extract fields to process
         let captures = password_regex.captures(line).unwrap();
         let lower = captures[1].parse::<usize>().unwrap();
         let upper = captures[2].parse::<usize>().unwrap();
-        let c = captures[3].to_string();
-        let password = captures[4].to_string();
+        let c = captures[3].chars().next().unwrap();
+        let password = captures[4].chars().collect::<Vec<char>>();
         output.push((lower, upper, c, password));
     }
     return output;
 }
 
 #[aoc(day2, part1)]
-fn solve_part_1(input: &Vec<(usize, usize, String, String)>) -> u64 {
+fn solve_part_1(input: &Vec<(usize, usize, char, Vec<char>)>) -> u64 {
     let mut valid_count = 0;
     for (lower, upper, c, password) in input {
         // Count number of times check character occurs in password
-        let count = &password.matches(c).count();
+        let count = &password.iter().filter(|x| *x == c).count();
         // Increase valid count if count within specified range (inclusive lower and upper)
         if count >= lower && count <= upper {
             valid_count += 1;
@@ -34,7 +36,7 @@ fn solve_part_1(input: &Vec<(usize, usize, String, String)>) -> u64 {
 }
 
 #[aoc(day2, part2)]
-fn solve_part_2(input: &Vec<(usize, usize, String, String)>) -> u64 {
+fn solve_part_2(input: &Vec<(usize, usize, char, Vec<char>)>) -> u64 {
     let mut valid_count = 0;
     for (lower, upper, c, password) in input {
         // Adjust lower and upper indices to accomodate for off-by-one
@@ -42,10 +44,10 @@ fn solve_part_2(input: &Vec<(usize, usize, String, String)>) -> u64 {
         let upper = upper - 1;
         let mut count = 0;
         // Check if validation character present at lower and upper indices
-        if lower <= password.len() - 1 && &password[lower..lower+1] == c {
+        if lower <= password.len() - 1 && &password[lower] == c {
             count += 1;
         }
-        if upper <= password.len() - 1 && &password[upper..upper+1] == c {
+        if upper <= password.len() - 1 && &password[upper] == c {
             count += 1;
         }
         // Increment valid count if check character is at exactly one of the specified indices

5464d299344ed933ffb032218732798c6d3532bd
Connor Mooney-Collett <connor.mooneycollett@gmail.com>
2020-12-03 18:00:53 +1100

    Solved Day 3 Part 2
    Also added Day 3 answer test cases.

--- src/day_03.rs
+++ src/day_03.rs
@@ -14,11 +14,14 @@ fn generate_input(input: &str) -> (HashMap<Point2D, MapTile>, i64, i64) {
     let mut y = 0;
     let mut forest_map: HashMap<Point2D, MapTile> = HashMap::new();
     for line in input.lines() {
+        // Back at start of new line
         x = 0;
+        // Remove leading and trailing whitespace, then ignore empty lines
         let line = line.trim();
         if line.is_empty() {
             continue;
         }
+        // Record element at current location on map
         for c in line.chars() {
             let loc = Point2D::new(x, y);
             match c {
@@ -44,11 +47,41 @@ fn solve_part_1(input: &(HashMap<Point2D, MapTile>, i64, i64)) -> u64 {
     let width = input.1;
     let height = input.2;
     let forest_map = input.0.clone();
+    return ride_through_forest(&forest_map, height, width, 3, 1);
+}
+
+#[aoc(day3, part2)]
+fn solve_part_2(input: &(HashMap<Point2D, MapTile>, i64, i64)) -> u64 {
+    let forest_map = input.0.clone();
+    let width = input.1;
+    let height = input.2;
+    // Initialise the slopes we need to check
+    let slopes: Vec<(i64, i64)> = vec![(1, 1), (3, 1), (5, 1), (7, 1), (1, 2)];
+    // Determine number of trees encountered on each slope
+    let mut result_record: Vec<u64> = vec![];
+    for (delta_x, delta_y) in slopes {
+        let trees_encountered = ride_through_forest(&forest_map, height, width, delta_x, delta_y);
+        result_record.push(trees_encountered);
+    }
+    // Determine the product of all results from forest rides for given slopes
+    let mut output = 1;
+    for result in result_record {
+        output *= result;
+    }
+    return output;
+}
+
+/// Conducts a toboggan ride through the given forest map (specified width and height) with slope
+/// defined by given x and y deltas per step.
+fn ride_through_forest(forest_map: &HashMap<Point2D, MapTile>, height: i64, width: i64, delta_x: i64,
+        delta_y: i64) -> u64
+{
+    // Start the toboggan at top left map tile - (0, 0)
     let mut trees_encountered = 0;
     let mut current_loc = Point2D::new(0, 0);
     loop {
-        // Check if bottom of map has been reached
-        if current_loc.get_y() == height {
+        // Check if bottom of map has been reached or exceeded
+        if current_loc.get_y() >= height {
             return trees_encountered;
         }
         // Update x-coord of current location to wrap around
@@ -59,6 +92,25 @@ fn solve_part_1(input: &(HashMap<Point2D, MapTile>, i64, i64)) -> u64 {
             trees_encountered += 1;
         }
         // Move location down slope
-        current_loc.move_point(3, 1);
+        current_loc.move_point(delta_x, delta_y);
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_d03_p1_proper() {
+        let input = generate_input(&std::fs::read_to_string("./input/2020/day3.txt").unwrap());
+        let result = solve_part_1(&input);
+        assert_eq!(230, result);
+    }
+
+    #[test]
+    fn test_d03_p2_proper() {
+        let input = generate_input(&std::fs::read_to_string("./input/2020/day3.txt").unwrap());
+        let result = solve_part_2(&input);
+        assert_eq!(9533698720, result);
     }
 }
--- src/utils/map/point2d.rs
+++ src/utils/map/point2d.rs
@@ -1,3 +1,4 @@
+/// Represents a single point in two-dimensional Euclidean space.
 #[derive(Copy, Clone, Hash, PartialEq, Eq, Debug)]
 pub struct Point2D {
     x: i64,
@@ -5,6 +6,7 @@ pub struct Point2D {
 }
 
 impl Point2D {
+    /// Creates a new 2D point.
     pub fn new(x: i64, y: i64) -> Self {
         Self {
             x: x,
@@ -12,18 +14,27 @@ impl Point2D {
         }
     }
 
+    /// Gets the value of the x-coordinate.
     pub fn get_x(&self) -> i64 {
         return self.x;
     }
 
+    /// Updates the value of the x-coordinate.
     pub fn set_x(&mut self, x: i64) {
         self.x = x;
     }
 
+    /// Gets the value of the y-coordinate.
     pub fn get_y(&self) -> i64 {
         return self.y;
     }
 
+    /// Updates the value of the y-coordinate.
+    pub fn set_y(&mut self, y: i64) {
+        self.y = y;
+    }
+
+    /// Moves the point by the specified amount in the x- and y-directions.
     pub fn move_point(&mut self, delta_x: i64, delta_y: i64) {
         self.x += delta_x;
         self.y += delta_y;

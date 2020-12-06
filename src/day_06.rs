f394f4cf6015747c4e1873bcefbbef3e799a6731
Connor Mooney-Collett <connor.mooneycollett@gmail.com>
2020-12-06 17:27:15 +1100

    Refactor day_06.rs for readability

--- src/day_06.rs
+++ src/day_06.rs
@@ -5,23 +5,24 @@ fn generate_input(input: &str) -> Vec<Vec<String>> {
     let mut groups: Vec<Vec<String>> = vec![];
     let mut lines = input.lines();
     loop {
-        let mut end_of_file = false;
+        let mut end_of_input = false;
         let mut group: Vec<String> = vec![];
         loop {
+            // Check if end of input reached (no more lines to read)
             let line = lines.next();
             if line.is_none() {
-                end_of_file = true;
+                end_of_input = true;
                 break;
             }
-            let line = line.unwrap().trim();
             // Check if reached end of current group, but not EOF yet
+            let line = line.unwrap().trim();
             if line.is_empty() {
                 break;
             }
             group.push(line.to_string());
         }
         groups.push(group);
-        if end_of_file {
+        if end_of_input {
             return groups;
         }
     }
@@ -31,13 +32,13 @@ fn generate_input(input: &str) -> Vec<Vec<String>> {
 fn solve_part_1(groups: &Vec<Vec<String>>) -> usize {
     let mut yes_total = 0;
     for group in groups {
-        let mut unique_question: HashSet<char> = HashSet::new();
+        let mut group_seen_yes: HashSet<char> = HashSet::new();
         for entry in group {
             for c in entry.chars() {
-                unique_question.insert(c);
+                group_seen_yes.insert(c);
             }
         }
-        yes_total += unique_question.len();
+        yes_total += group_seen_yes.len();
     }
     return yes_total;
 }
@@ -46,25 +47,27 @@ fn solve_part_1(groups: &Vec<Vec<String>>) -> usize {
 fn solve_part_2(groups: &Vec<Vec<String>>) -> usize {
     let mut common_yes_total = 0;
     for group in groups {
-        let mut record: HashSet<char> = HashSet::new();
+        let mut group_common_yes: HashSet<char> = HashSet::new();
         for i in 0..group.len() {
-            let entry_chars = group[i].chars().collect::<HashSet<char>>();
-            if i == 0 { // Initialise yes record on first first entry
-                record = entry_chars.clone();
+            // Determine the unique characters in the current group entry (form)
+            let entry_uniq_chars = group[i].chars().collect::<HashSet<char>>();
+            if i == 0 { // Initialise yes record on first group entry
+                group_common_yes = entry_uniq_chars.clone();
             } else {
                 // Check which yes questions seen common so far are not present in current entry
                 let mut c_to_remove: Vec<char> = vec![];
-                for c in record.iter() {
-                    if !entry_chars.contains(&c) {
+                for c in group_common_yes.iter() {
+                    if !entry_uniq_chars.contains(&c) {
                         c_to_remove.push(*c);
                     }
                 }
+                // Remove elements from group common that were in current entry but not seen yet
                 for c in c_to_remove {
-                    record.remove(&c);
+                    group_common_yes.remove(&c);
                 }
             }
         }
-        common_yes_total += record.len();
+        common_yes_total += group_common_yes.len();
     }
     return common_yes_total;
 }

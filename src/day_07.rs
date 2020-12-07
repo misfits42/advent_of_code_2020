6ecf077bb55730e8342a9211fcb79c741c9f2b07
Connor Mooney-Collett <connor.mooneycollett@gmail.com>
2020-12-07 18:59:24 +1100

    Tidy up Day 7 solution for readability

--- src/day_07.rs
+++ src/day_07.rs
@@ -4,40 +4,45 @@ use regex::Regex;
 
 #[aoc_generator(day7)]
 fn generate_input(input: &str) -> HashMap<String, HashMap<String, u64>> {
-    let mut bag_contents: HashMap<String, HashMap<String, u64>> = HashMap::new();
+    let mut bag_rules: HashMap<String, HashMap<String, u64>> = HashMap::new();
     let bag_no_contents_regex = Regex::new(r"^(.*) bags contain no other bags.$").unwrap();
     let bag_with_contents_regex = Regex::new(r"^(.*) bags contain (.*).$").unwrap();
-    let bag_spec_regex = Regex::new(r"(\d+) (.*) bag[s]?").unwrap();
+    let inner_bag_regex = Regex::new(r"(\d+) (.*) bag[s]?").unwrap();
     for line in input.lines() {
+        // Trim leading and trailing whitespace, then skip empty lines.
         let line = line.trim();
         if line.is_empty() {
             continue;
         }
-        let mut contents: HashMap<String, u64> = HashMap::new();
+        let mut inner_bags: HashMap<String, u64> = HashMap::new();
+        // Match rule for bag containing no other bags
         if bag_no_contents_regex.is_match(line) {
             let captures = bag_no_contents_regex.captures(line).unwrap();
             let bag_type = captures[1].to_string();
-            bag_contents.insert(bag_type, contents);
+            bag_rules.insert(bag_type, inner_bags);
+        // Match rule for bag containing other bags
         } else if bag_with_contents_regex.is_match(line) {
             let captures = bag_with_contents_regex.captures(line).unwrap();
             let bag_type = captures[1].to_string();
+            // Extract type and quantity for inner bags
             for inner_bag_spec in captures[2].split(", ") {
-                let inner_bag_captures = bag_spec_regex.captures(inner_bag_spec).unwrap();
+                let inner_bag_captures = inner_bag_regex.captures(inner_bag_spec).unwrap();
                 let quantity = inner_bag_captures[1].parse::<u64>().unwrap();
                 let inner_bag_type = inner_bag_captures[2].to_string();
-                contents.insert(inner_bag_type, quantity);
+                inner_bags.insert(inner_bag_type, quantity);
             }
-            bag_contents.insert(bag_type, contents);
+            bag_rules.insert(bag_type, inner_bags);
         } else {
             panic!("Day 7 - malformed input file!");
         }
     }
-    return bag_contents;
+    return bag_rules;
 }
 
 #[aoc(day7, part1)]
 fn solve_part_1(bag_rules: &HashMap<String, HashMap<String, u64>>) -> u64 {
     let mut count = 0;
+    // For each specified bag type, check if it eventually contains a "shiny gold" bag
     for bag_type in bag_rules.keys() {
         if check_for_inner_bag(bag_rules, bag_type, "shiny gold") {
             count += 1;
@@ -55,17 +60,16 @@ fn solve_part_2(bag_rules: &HashMap<String, HashMap<String, u64>>) -> u64 {
 fn check_for_inner_bag(bag_rules: &HashMap<String, HashMap<String, u64>>, current_bag: &str,
         target_bag: &str) -> bool
 {
-    let mut result = false;
     if !bag_rules.contains_key(current_bag) {
         return false;
     }
     for (inner_bag, _quantity) in bag_rules.get(current_bag).unwrap() {
-        if inner_bag == target_bag {
+        // Check if the current inner bag is the target bag or contains the target bag
+        if inner_bag == target_bag || check_for_inner_bag(bag_rules, inner_bag, target_bag) {
             return true;
         }
-        result |= check_for_inner_bag(bag_rules, inner_bag, target_bag);
     }
-    return result;
+    return false;
 }
 
 /// Counts the number of bags contained within the current bag.

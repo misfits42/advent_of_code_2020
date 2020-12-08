bfdba84438380517c3c585690d1fb3d0f8c32560
Connor Mooney-Collett <connor.mooneycollett@gmail.com>
2020-12-08 19:32:05 +1100

    Solved Day 8 Part 2

--- src/day_08.rs
+++ src/day_08.rs
@@ -13,6 +13,44 @@ fn generate_input(input: &str) -> Vec<HandheldConsoleInstruction> {
 #[aoc(day8, part1)]
 fn solve_part_1(instructions: &Vec<HandheldConsoleInstruction>) -> isize {
     let mut handheld_console = HandheldConsole::new(instructions);
-    handheld_console.execute();
-    return handheld_console.gen_accumulator();
+    handheld_console.execute(0, true);
+    return handheld_console.get_accumulator();
+}
+
+#[aoc(day8, part2)]
+fn solve_part_2(instructions: &Vec<HandheldConsoleInstruction>) -> isize {
+    let mut handheld_console = HandheldConsole::new(instructions);
+    loop {
+        // Try to execute
+        handheld_console.execute(1000, false);
+        // Check if the handheld console halted
+        if handheld_console.is_halted() {
+            return handheld_console.get_accumulator();
+        }
+        // Check if toggles exhausted
+        if handheld_console.check_toggles_exhausted() {
+            panic!("Day 8 Part 2 - toggles exhausted!");
+        }
+        // Did not halt within set number of steps, to toggle next JMP or NOP
+        handheld_console.toggle_next_jmp_or_nop();
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_d08_p1_proper() {
+        let input = generate_input(&std::fs::read_to_string("./input/2020/day8.txt").unwrap());
+        let result = solve_part_1(&input);
+        assert_eq!(2058, result);
+    }
+
+    #[test]
+    fn test_d08_p2_proper() {
+        let input = generate_input(&std::fs::read_to_string("./input/2020/day8.txt").unwrap());
+        let result = solve_part_2(&input);
+        assert_eq!(1000, result);
+    }
 }
--- src/utils/machines/handheldconsole.rs
+++ src/utils/machines/handheldconsole.rs
@@ -10,6 +10,14 @@ pub enum HandheldConsoleInstruction {
 }
 
 impl HandheldConsoleInstruction {
+    pub fn toggle(&self) -> Self {
+        match self {
+            HandheldConsoleInstruction::Acc{arg} => return HandheldConsoleInstruction::Acc{arg: *arg},
+            HandheldConsoleInstruction::Jmp{arg} => return HandheldConsoleInstruction::Nop{arg: *arg},
+            HandheldConsoleInstruction::Nop{arg} => return HandheldConsoleInstruction::Jmp{arg: *arg},
+        }
+    }
+
     /// Parses the provided input for instructions, assuming that each instruction is on its own
     /// line.
     pub fn parse_raw_code(input: &str) -> Option<Vec<HandheldConsoleInstruction>> {
@@ -44,7 +52,9 @@ impl HandheldConsoleInstruction {
 
 pub struct HandheldConsole {
     instructions: Vec<HandheldConsoleInstruction>,
+    toggle_index: Option<usize>,
     accumulator: isize,
+    halted: bool,
     exec_count: u64,
     prog_counter: isize,
     history: HashMap<usize, Vec<u64>>
@@ -54,43 +64,93 @@ impl HandheldConsole {
     pub fn new(instructions: &Vec<HandheldConsoleInstruction>) -> Self {
         Self {
             instructions: instructions.clone(),
+            toggle_index: Some(0),
             accumulator: 0,
+            halted: false,
             exec_count: 0,
             prog_counter: 0,
             history: HashMap::new()
         }
     }
 
-    pub fn gen_accumulator(&self) -> isize {
+    pub fn toggle_next_jmp_or_nop(&mut self) {
+        // Toggle back the last instruction toggled
+        if self.toggle_index.unwrap() > 0 {
+            self.instructions[self.toggle_index.unwrap() - 1] = self.instructions[self.toggle_index.unwrap() - 1].toggle();
+        }
+        self.accumulator = 0;
+        self.halted = false;
+        self.exec_count = 0;
+        self.prog_counter = 0;
+        self.history = HashMap::new();
+        for i in self.toggle_index.unwrap()..self.instructions.len() {
+            match self.instructions[i] {
+                HandheldConsoleInstruction::Acc{arg: _} => (),
+                _ => {
+                    self.instructions[i] = self.instructions[i].toggle();
+                    self.toggle_index = Some(i + 1);
+                    return;
+                },
+            }
+        }
+        self.toggle_index = None;
+    }
+
+    pub fn is_halted(&self) -> bool {
+        return self.halted;
+    }
+
+    pub fn check_toggles_exhausted(&self) -> bool {
+        return self.toggle_index.is_none();
+    }
+
+    pub fn get_accumulator(&self) -> isize {
         return self.accumulator;
     }
 
+    fn execute_single_instruction(&mut self, pause_on_history: bool) {
+        if self.halted == true {
+            return;
+        }
+        // Check if program counter is within instruction space
+        if self.prog_counter < 0 || self.prog_counter as usize >= self.instructions.len() {
+            self.halted = true;
+            return;
+        }
+        // Check if current instruction would be executed for a second time
+        if pause_on_history && self.history.contains_key(&(self.prog_counter as usize)) {
+            self.halted = true;
+            return;
+        }
+        // Add program counter to history
+        self.history.insert(self.prog_counter as usize, vec![self.exec_count]);
+        // Now execute the instruction
+        match self.instructions[self.prog_counter as usize] {
+            HandheldConsoleInstruction::Acc{arg} => {
+                self.accumulator += arg;
+                self.prog_counter += 1;
+            },
+            HandheldConsoleInstruction::Jmp{arg} => {
+                self.prog_counter += arg;
+            },
+            HandheldConsoleInstruction::Nop{arg: _} => {
+                self.prog_counter += 1;
+            }
+        }
+    }
+
     /// Executes instructions until an instruction would be executed for a second time (but)
-    pub fn execute(&mut self) {
+    pub fn execute(&mut self, steps: usize, pause_on_history: bool) {
+        let mut step = 0;
         loop {
-            // Check if program counter is within instruction space
-            if self.prog_counter < 0 || self.prog_counter as usize >= self.instructions.len() {
+            if !pause_on_history && step >= steps {
                 return;
             }
-            // Check if current instruction would be executed for a second time
-            if self.history.contains_key(&(self.prog_counter as usize)) {
+            if self.halted == true {
                 return;
             }
-            // Add program counter to history
-            self.history.insert(self.prog_counter as usize, vec![self.exec_count]);
-            // Now execute the instruction
-            match self.instructions[self.prog_counter as usize] {
-                HandheldConsoleInstruction::Acc{arg} => {
-                    self.accumulator += arg;
-                    self.prog_counter += 1;
-                },
-                HandheldConsoleInstruction::Jmp{arg} => {
-                    self.prog_counter += arg;
-                },
-                HandheldConsoleInstruction::Nop{arg: _} => {
-                    self.prog_counter += 1;
-                }
-            }
+            step += 1;
+            self.execute_single_instruction(pause_on_history);
         }
     }
 }

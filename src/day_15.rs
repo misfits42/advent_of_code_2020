d2fbeab99d19f97ef6f8719d3feb2214450882f5
Connor Mooney-Collett <connor.mooneycollett@gmail.com>
2020-12-22 15:07:16 +1000

    Solved Day 15 Part 2
    All provided test cases pass when executed.

--- src/day_15.rs
+++ src/day_15.rs
@@ -7,11 +7,23 @@ fn generate_input(input: &str) -> Vec<u64> {
 
 #[aoc(day15, part1)]
 fn solve_part_1(starting_numbers: &Vec<u64>) -> u64 {
+    return play_memory_game(2020, starting_numbers);
+}
+
+#[aoc(day15, part2)]
+fn solve_part_2(starting_numbers: &Vec<u64>) -> u64 {
+    return play_memory_game(30000000, starting_numbers);
+}
+
+/// Plays the Elves' memory game for the specified number of turns using the given starting numbers.
+/// 
+/// Rules as described in AOC 2020 Day 15.
+fn play_memory_game(num_turns: usize, starting_numbers: &Vec<u64>) -> u64 {
     // Record the numbers spoken so far and on what turns they have been spoken
     let mut nums_spoken: HashMap<u64, Vec<u64>> = HashMap::new();
     let mut last_num_spoken = 0;
     // Conduct the turns
-    for current_turn in 0..2020 {
+    for current_turn in 0..num_turns {
         // Determine the number to be spoken on current turn
         let number_spoken = {
             if current_turn < starting_numbers.len() {
@@ -51,6 +63,15 @@ mod tests {
         );
         let result = solve_part_1(&input);
         assert_eq!(371, result);
+    }
+
+    #[test]
+    fn test_d15_p2_proper() {
+        let input = generate_input(
+            &std::fs::read_to_string("./input/2020/day15.txt").unwrap(),
+        );
+        let result = solve_part_2(&input);
+        assert_eq!(352, result);
     }
 
     #[test]
@@ -114,5 +135,68 @@ mod tests {
         );
         let result = solve_part_1(&input);
         assert_eq!(1836, result);
+    }
+
+    #[test]
+    fn test_d15_p2_001() {
+        let input = generate_input(
+            &std::fs::read_to_string("./input/2020/test/day15_test_001.txt").unwrap(),
+        );
+        let result = solve_part_2(&input);
+        assert_eq!(175594, result);
+    }
+
+    #[test]
+    fn test_d15_p2_002() {
+        let input = generate_input(
+            &std::fs::read_to_string("./input/2020/test/day15_test_002.txt").unwrap(),
+        );
+        let result = solve_part_2(&input);
+        assert_eq!(2578, result);
+    }
+
+    #[test]
+    fn test_d15_p2_003() {
+        let input = generate_input(
+            &std::fs::read_to_string("./input/2020/test/day15_test_003.txt").unwrap(),
+        );
+        let result = solve_part_2(&input);
+        assert_eq!(3544142, result);
+    }
+
+    #[test]
+    fn test_d15_p2_004() {
+        let input = generate_input(
+            &std::fs::read_to_string("./input/2020/test/day15_test_004.txt").unwrap(),
+        );
+        let result = solve_part_2(&input);
+        assert_eq!(261214, result);
+    }
+
+    #[test]
+    fn test_d15_p2_005() {
+        let input = generate_input(
+            &std::fs::read_to_string("./input/2020/test/day15_test_005.txt").unwrap(),
+        );
+        let result = solve_part_2(&input);
+        assert_eq!(6895259, result);
+    }
+
+    #[test]
+    fn test_d15_p2_006() {
+        let input = generate_input(
+            &std::fs::read_to_string("./input/2020/test/day15_test_006.txt").unwrap(),
+        );
+        let result = solve_part_2(&input);
+        assert_eq!(18, result);
+    }
+
+    #[test]
+    fn test_d15_p2_007() {
+        let input = generate_input(
+            &std::fs::read_to_string("./input/2020/test/day15_test_007.txt").unwrap(),
+        );
+        let result = solve_part_2(&input);
+        assert_eq!(362, result);
     }
 }

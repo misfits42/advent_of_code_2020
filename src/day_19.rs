356fcd9883479d7acc813d9d7ad4f155cdea1761
Connor Mooney-Collett <connor.mooneycollett@gmail.com>
2020-12-26 07:32:59 +1000

    Solved Day 19 Part 1

--- src/day_19.rs
+++ src/day_19.rs
@@ -16,7 +16,10 @@ fn generate_input(input: &str) -> (HashMap<u64, String>, Vec<String>) {
         .map(|x| (x[1].parse::<u64>().unwrap(), x[2].to_string()))
         .collect::<HashMap<u64, String>>();
     // Extract messages
-    let messages = segments[1].lines().map(|x| x.trim().to_string()).collect::<Vec<String>>();
+    let messages = segments[1]
+        .lines()
+        .map(|x| x.trim().to_string())
+        .collect::<Vec<String>>();
     return (rules_raw, messages);
 }
 
@@ -25,7 +28,11 @@ fn solve_part_1(input: &(HashMap<u64, String>, Vec<String>)) -> u64 {
     let rules_raw = input.0.clone();
     let messages = input.1.clone();
     // Generate regex for rule 0
-    let rule_0_regex_str = generate_regex_str_from_rules(0, &rules_raw).unwrap();
+    let rule_0_regex_str = format!(
+        "^{}$",
+        generate_regex_str_from_rules(0, &rules_raw).unwrap()
+    );
+    // println!("Regex str ---- {}", rule_0_regex_str);
     let regex = Regex::new(&rule_0_regex_str).unwrap();
     let mut valid_count = 0;
     for message in messages {
@@ -36,7 +43,10 @@ fn solve_part_1(input: &(HashMap<u64, String>, Vec<String>)) -> u64 {
     return valid_count;
 }
 
-fn generate_regex_str_from_rules(rule_num: u64, rules_raw: &HashMap<u64, String>) -> Option<String> {
+fn generate_regex_str_from_rules(
+    rule_num: u64,
+    rules_raw: &HashMap<u64, String>,
+) -> Option<String> {
     // Check if the current rule number exists
     if !rules_raw.contains_key(&rule_num) {
         return None;
@@ -46,37 +56,53 @@ fn generate_regex_str_from_rules(rule_num: u64, rules_raw: &HashMap<u64, String>
     let mut regex_str = rule_text.clone();
     // Check if there are any sub-rules that need to be substituted
     if rule_text.starts_with("\"") {
-        // println!("reached a base case ---- rule_num: {}, rule_text: {}", rule_num, rule_text);
         return Some(rule_text.replace("\"", ""));
     }
     let rule_num_regex = Regex::new(r"(\d+)*").unwrap();
-    //let captures = rule_num_regex.captures_iter()(&rule_text).unwrap();
-    // println!("[?] rule_num: {} ---- rule_text: {}", rule_num, rule_text);
     for cap in rule_num_regex.captures_iter(&rule_text) {
-        // println!("[-] cap: {:?}", cap);
         if cap.len() != 2 || cap.get(1).is_none() {
             continue;
         }
-        // println!("pre new rule num");
         let new_rule_num = cap[1].parse::<u64>().unwrap();
-        // println!("new rule num: {}", new_rule_num);
         let sub_regex = generate_regex_str_from_rules(new_rule_num, rules_raw).unwrap();
-        let replace_regex = Regex::new(&format!(r"(\s+({})\s+)|(^({})\s+)|(\s+({})$)", &cap[1],&cap[1],&cap[1])).unwrap();
+        let replace_regex = Regex::new(&format!(
+            r"(\s+({})\s+)|(^({})\s+)|(\s+({})$)|^({})$",
+            &cap[1], &cap[1], &cap[1], &cap[1]
+        ))
+        .unwrap();
         regex_str = {
-            // println!(">>>>>>>>>> cap[1] = {}", &cap[1]);
-            // println!("{}", regex_str);
             if sub_regex.len() == 1 {
-                // regex_str.replace(&cap[1], &format!("{}", sub_regex))
-                replace_regex.replace_all(&regex_str, |_caps: &Captures| {
-                    format!(" {} ", sub_regex)
-                }).to_string()
+                replace_regex
+                    .replace_all(&regex_str, |_caps: &Captures| format!(" {} ", sub_regex))
+                    .to_string()
             } else {
-                replace_regex.replace_all(&regex_str, |_caps: &Captures| {
-                    format!(" ({}) ", sub_regex)
-                }).to_string()
+                replace_regex
+                    .replace_all(&regex_str, |_caps: &Captures| {
+                        format!(" ( {} ) ", sub_regex)
+                    })
+                    .to_string()
             }
         };
     }
     regex_str = regex_str.replace(" ", "");
     return Some(regex_str);
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_d19_p1_proper() {
+        let input = generate_input(&std::fs::read_to_string("./input/2020/day19.txt").unwrap());
+        let result = solve_part_1(&input);
+        assert_eq!(184, result);
+    }
+
+    #[test]
+    fn test_d19_p1_002() {
+        let input = generate_input(&std::fs::read_to_string("./input/2020/test/day19_test_002.txt").unwrap());
+        let result = solve_part_1(&input);
+        assert_eq!(2, result);
+    }
 }
